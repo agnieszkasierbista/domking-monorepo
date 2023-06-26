d0155e7ce64d5443618f49b52271ce19dc0bb3c7
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2023-06-26 12:04:08 +0200

    Initiate a simple React component library

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/components/index.ts',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {test: /\.css$/, use: 'css-loader'},
+            {
+                test: /\.tsx?$/, use: {
+                    loader: "ts-loader",
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx', '.jsx']
+    },
+};
