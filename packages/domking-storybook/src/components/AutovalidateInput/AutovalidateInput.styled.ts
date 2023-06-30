0d1e58ecd6c726c95e78fb4aea8325dd99fcff1c
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2023-06-30 12:06:43 +0200

    Import domking module to domking-storybook storybook

--- packages/domking-storybook/src/components/AutovalidateInput/AutovalidateInput.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledAutoValidateInput = styled.div<{}>`
-
-`;
-
-export const StyledTextInput = styled.input<{ $isValid: boolean | undefined }>`
-  outline: ${(props) => props.$isValid === false ? "4px solid red" : ""};
-`;
